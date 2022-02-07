42d1d663b7f14efbb55cc7dc5244bcedb74e18eb
leonardo mosso <leonardo_mcv@hotmail.com>
2022-02-07 12:23:56 -0300

    initial project

---
+++ react_translate_page/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import InitialPage from "./pages/InitialPage";
+
+
+function App() {
+  return (
+    <div className="App">
+      <InitialPage />
+    </div>
+  );
+}
+
+export default App;
